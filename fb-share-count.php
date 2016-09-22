70bb71ae3a8cc22ba22cd88c53f1fa9c1f1509b8
Yousan_O <yousan@l2tp.org>
2016-09-22 19:24:59 +0900

    Initial commit

---
+++ fb-share-count.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Fb Share Count
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     fb-share-count
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fb_Share_Count
+ */
